4d5aa254e15c1459cd4ad538a9549dd6161327b8
ZsebeSzilard <zsebeszilard98@gmail.com>
2021-12-10 15:45:28 +0200

    End of section 16

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -36,7 +36,10 @@ export class AccountService {
     )
   }
 
-  setCurrentUser(user:User|null){
+  setCurrentUser(user:User | null){
+    user!.roles = [];
+    const roles = this.getDecodedToken(user!.token).role;
+    Array.isArray(roles) ? user!.roles = roles : user!.roles.push(roles);
     localStorage.setItem('user',JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -44,5 +47,10 @@ export class AccountService {
   logout(){
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token:string)
+  {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
